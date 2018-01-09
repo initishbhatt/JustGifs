36cc134c5b83e0b991d55b3ceef4f4289a8f3b87
Nitish Bhatt <nitishkbhatt@gmail.com>
2018-01-09 15:43:37 +0530

    refactor-api-key

--- app/src/main/java/initishbhatt/com/justgifs/gifs/api/GifService.kt
+++ app/src/main/java/initishbhatt/com/justgifs/gifs/api/GifService.kt
@@ -10,6 +10,6 @@ import retrofit2.http.Query
  * @author nitishbhatt
  */
 interface GifService {
-    @GET("/v1/gifs/trending?api_key" + BuildConfig.API_KEY)
+    @GET("/v1/gifs/trending?api_key=" + BuildConfig.API_KEY)
     fun getTrendingGifs(@Query("limit") limit: Int, @Query("rating") rating: String): Single<TrendingGifResponse>
 }
